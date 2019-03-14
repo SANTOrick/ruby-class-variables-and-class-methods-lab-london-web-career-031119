d651076b255f61b9912ada12a4dca538e155fb4f
Riccardo <riccardo_london@live.it>
2019-03-14 16:06:37 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+class Song
+attr_accessor :name, :artist, :genre, :count
+
+    @@artists = []
+    @@genres = []
+    @@genre_count = 0
+    @@count = 0
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@artists.push artist
+    @@genres.push genre
+    @@count += 1
+  end
+
+  def new (name, artist, genre)
+
+  end
+
+  def artists
+
+  end
+
+
+
+end
